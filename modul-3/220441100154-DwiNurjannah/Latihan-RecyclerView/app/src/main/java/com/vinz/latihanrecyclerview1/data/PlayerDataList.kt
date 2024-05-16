8f2aeca05b66379a30f152b79f38bfeb9b4d6a86
DWINURJANNAH <113224017+DWINURJANNAH@users.noreply.github.com>
2024-05-16 23:48:25 +0700

    220441100154-DWI NURJANNAH-NUR MAULANA ARRAFI-MODUL 3

---
+++ modul-3/220441100154-DwiNurjannah/Latihan-RecyclerView/app/src/main/java/com/vinz/latihanrecyclerview1/data/PlayerDataList.kt
@@ -0,0 +1,32 @@
+package com.vinz.latihanrecyclerview1.data
+
+import com.vinz.latihanrecyclerview1.R
+
+// Dummy data yang nantinya akan kita tampilkan di dalam Aplikasi
+object PlayerDataList {
+
+    val peopledata = arrayListOf(
+        peopledata(
+            "sipak",
+            "Mandi Besar"
+        ),
+        peopledata(
+            "sipak",
+            "Mandi Besar"
+        ),
+        peopledata(
+            "sipak",
+            "Mandi Besar"
+        ),
+        peopledata(
+            "sipak",
+            "Mandi Besar"
+        ),
+        peopledata(
+            "sipak",
+            "Mandi Besar"
+        )
+    )
+
+}
+
